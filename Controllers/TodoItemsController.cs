9fbafeca51df1251c0e3669e1708a3d7146701b3
Lukhas Daniel <lukhasfurtado9@gmail.com>
2024-02-21 16:25:49 -0300

    Atualizado método Post

--- Controllers/TodoItemsController.cs
+++ Controllers/TodoItemsController.cs
@@ -80,7 +80,8 @@ namespace web_API_with_ASP.NET_Core.Controllers
             _context.TodoItems.Add(todoItem);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            //    return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
         }
 
         // DELETE: api/TodoItems/5
